6a014645b91056f4f5810e9b015cf1fc42c5e1c0
fikiryilkal tages <fikeryilkaltages@gmail.com>
2025-11-06 21:38:34 +0300

    vector search

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse, NextRequest } from "next/server";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { embedTextGemini } from "@/lib/embed";
+import RagChunks from "@/model/ragChunks";
+import dbConnect from "@/lib/mongoose";
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
@@ -9,6 +12,8 @@ const model = new ChatGoogleGenerativeAI({
 
 export async function POST(req: NextRequest) {
   try {
+    await dbConnect();
+
     const { prompt } = await req.json();
     if (!prompt) {
       return NextResponse.json(
@@ -17,9 +22,59 @@ export async function POST(req: NextRequest) {
       );
     }
 
-    const response = await model.invoke(prompt);
+    const queryEmbedding = await embedTextGemini({
+      contents: [prompt],
+    });
 
-    return NextResponse.json({ response: response.text });
+    const vector = queryEmbedding[0].values;
+    const similarChunks = await RagChunks.aggregate([
+      {
+        $vectorSearch: {
+          queryVector: vector || [],
+          index: "rga_index",
+          path: "embedding",
+          numCandidates: 50,
+          limit: 5,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          docName: 1,
+          chunkText: 1,
+          score: { $meta: "vectorSearchScore" },
+        },
+      },
+    ]);
+
+    const contextBlocks = (similarChunks || []).map(
+      (c: { docName: string; chunkText: string; score?: number }, i: number) =>
+        `Source ${i + 1} (${c.docName}):\n${c.chunkText}`
+    );
+
+    const contextText = contextBlocks.join("\n\n");
+
+    const finalPrompt = `You are a helpful assistant. Use ONLY the provided context to answer the user's question. If the answer is not in the context, say you don't know.
+
+    Context:
+    ${contextText || "<no context available>"}
+
+    User question:
+    ${prompt}
+
+    Answer`;
+
+    const response = await model.invoke(finalPrompt);
+
+    return NextResponse.json({
+      response: response.text,
+      usedChunks:
+        similarChunks?.map((c: any, i: number) => ({
+          i,
+          docName: c.docName,
+          score: c.score,
+        })) ?? [],
+    });
   } catch (error) {
     console.error("Error generating content:", error);
     return NextResponse.json(
